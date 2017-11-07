b75daeeab5e184a72a0b5bca9fa9565883d040f9
AZZ <806622085@qq.com>
2017-11-07 10:54:33 +0800

    Initial Commit

---
+++ AZCommonUsed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AZCommonUsed
+//
+//  Created by cocozzhang on 2017/11/7.
+//  Copyright © 2017年 azz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AZCommonUsed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AZCommonUsed
+//
+//  Created by cocozzhang on 2017/11/7.
+//  Copyright © 2017年 azz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
