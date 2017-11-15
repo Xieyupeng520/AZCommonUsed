1c99da81169b3d4f4eec2eaa18ad497ad73b0bfe
AZZ <806622085@qq.com>
2017-11-15 19:36:55 +0800

    添加常用字号

--- Font/字体默认留白/FontGapViewController.m
+++ Font/字体默认留白/FontGapViewController.m
@@ -18,17 +18,22 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
-    UILabel* label1 = [self createLabel:@"空隙测试：22px" fontSize:11 frontLabel:nil];
-    UILabel* label2 = [self createLabel:@"空隙测试：24px" fontSize:12 frontLabel:label1];
-    UILabel* label3 = [self createLabel:@"空隙测试：28px" fontSize:14 frontLabel:label2];
-    UILabel* label4 = [self createLabel:@"空隙测试：36px" fontSize:18 frontLabel:label3];
-    UILabel* label5 = [self createLabel:@"空隙测试：48px" fontSize:24 frontLabel:label4];
-    UILabel* label6 = [self createLabel:@"空隙测试：56px" fontSize:28 frontLabel:label5];
-    UILabel* label7 = [self createLabel:@"空隙测试：86px" fontSize:43 frontLabel:label6];
+    UILabel* label22 = [self createLabel:@"空隙测试：22px" fontSize:11 frontLabel:nil];
+    UILabel* label24 = [self createLabel:@"空隙测试：24px" fontSize:12 frontLabel:label22];
+    UILabel* label28 = [self createLabel:@"空隙测试：28px" fontSize:14 frontLabel:label24];
+    UILabel* label36 = [self createLabel:@"空隙测试：36px" fontSize:18 frontLabel:label28];
+    UILabel* label38 = [self createLabel:@"空隙测试：38px" fontSize:19 frontLabel:label36];
+    UILabel* label48 = [self createLabel:@"空隙测试：48px" fontSize:24 frontLabel:label38];
+    UILabel* label52 = [self createLabel:@"空隙测试：52px" fontSize:26 frontLabel:label48];
+    UILabel* label56 = [self createLabel:@"空隙测试：56px" fontSize:28 frontLabel:label52];
+    UILabel* label72 = [self createLabel:@"空隙测试：72px" fontSize:36 frontLabel:label56];
+    UILabel* label86 = [self createLabel:@"空隙测试：86px" fontSize:43 frontLabel:label72];
+    UILabel* label90 = [self createLabel:@"空隙测试：90px" fontSize:45 frontLabel:label86];
+    UILabel* label98 = [self createLabel:@"空隙测试：98px" fontSize:49 frontLabel:label90];
 }
 
 - (UILabel*)createLabel:(NSString*)text fontSize:(CGFloat)fontSize frontLabel:(UILabel*)labelFront {
-    int margin = 50;
+    int margin = 15;
     
     UILabel *label = [UILabel new];
     label.text = text;
@@ -44,4 +49,7 @@
     return label;
 }
 
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
 @end
